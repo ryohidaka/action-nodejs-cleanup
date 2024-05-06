34891d19c070faa8373c327cd9c5775cd23d3798
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-05-06 14:25:32 +0900

    feat: Add process to remove template files.

--- sh/replace/template-file.sh
+++ sh/replace/template-file.sh
@@ -6,3 +6,9 @@ do
   cp -f -r "$file" .
   echo "::notice:: Copied $file"
 done
+
+# Remove template dir
+if [ "$INPUT_REMOVE_TEMPLATE_DIR" = true ]; then
+  rm -rf "$INPUT_TEMPLATE_DIR"
+  echo "::notice:: Removed directory $INPUT_TEMPLATE_DIR"
+fi
